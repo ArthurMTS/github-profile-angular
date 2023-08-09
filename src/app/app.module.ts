194d30b949cd96704aa441f43ae3b280af703c02
Arthur Silva <arthurmatsilva@outlook.com>
2023-08-09 14:00:30 -0300

    chore: changing components folder from components to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SearchBar } from 'src/components/search-bar/search-bar.component';
-import { DisplayData } from 'src/components/display-data/display-data.component';
+import { SearchBar } from './search-bar/search-bar.component';
+import { DisplayData } from './display-data/display-data.component';
 
 @NgModule({
   declarations: [
