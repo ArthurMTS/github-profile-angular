285362104295f8135777050271f2d26bbfebcc5e
Arthur Silva <arthurmatsilva@outlook.com>
2023-08-09 15:28:40 -0300

    chore: request validation and conditional rendering of error message

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, /*OnInit*/ } from "@angular/core";
 import { ApiService, iUser } from "../api.service";
 
 @Component({
@@ -6,14 +6,26 @@ import { ApiService, iUser } from "../api.service";
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.scss'],
 })
-export class SearchBar {
+export class SearchBar /*implements OnInit*/ {
+  username = "";
   user: iUser = {} as iUser;
+  notFound = false;
 
   constructor(private apiService: ApiService) {}
 
   /*ngOnInit() {
-    this.apiService.getUser("ArthurMTS").subscribe((data: iUser) => {
-      this.user = data;
-    });
+    try {
+      this.apiService.getUser("ArthurMTS").subscribe((data: iUser) => {
+        this.user = data;
+        if (data?.message === "Not Found") {
+          this.found = true;
+          return;
+        }
+        this.found = false;
+      });
+    } catch(err) {
+      console.error(err);
+      this.found = true;
+    }
   }*/
 }
